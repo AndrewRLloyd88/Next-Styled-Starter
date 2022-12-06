52b4c261170c8b729d023ef400bc60fff25aff4c
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2022-12-05 17:06:11 -0800

    back to first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-const URL = process.env.STRAPIBASEURL;
-
-export async function getStaticProps(context) {
-  const fetchParams = {
-    method: 'post',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: `{
-        counsellers {
-         data {
-           attributes {
-             Name
-             Pronouns
-             Qualifications
-             Biography
-             DetailedBio
-             Headshot {
-               data {
-                 attributes {
-                   url
-                 }
-               }
-             }
-           }
-         }
-       }
-      }
-      `,
-    }),
-  };
-
-  const res = await fetch(`${URL}/graphql`, fetchParams);
-  const data = await res.json();
-
-  return {
-    props: data,
-  };
-}
-
-export default function Home({ data }) {
-  console.log(data);
+export default function Home() {
   return <div>Hello!</div>;
 }
